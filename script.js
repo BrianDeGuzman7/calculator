0e61960fe6eaa370d4761ee36ad5a47d45485a74
Brian Edwin DeGuzman <bradeguz@ucsc.edu>
2020-04-25 13:54:08 -0700

    Changed Project to Simple Calculator

---
+++ script.js
@@ -0,0 +1,15 @@
+//Add Function 
+const add = (a,b) => {
+    console.log(a+b);
+    return a + b;
+
+}
+
+const subtract = (a,b) => {
+    return a-b;
+}
+
+const multiply = (a,b) => {
+    return a*b;
+}
+
